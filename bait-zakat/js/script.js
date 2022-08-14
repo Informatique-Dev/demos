668f2e9419b4f0f574b895c0306c3716da18e8e2
ZinabHashem <zadel@informatique-eg.com>
2022-08-15 01:24:28 +0200

    second three pages

--- bait-zakat/js/script.js
+++ bait-zakat/js/script.js
@@ -3,3 +3,54 @@ function openForm() {
     
     document.getElementById("popupForm").style.display = "block";
   }
+
+function isWork() {
+  document.getElementById("salary").value = ''
+  document.getElementById("salary").disabled = !document.getElementById("salary").disabled;
+}
+
+// Customer table Data
+var customerContainer=[];
+var customerName = document.getElementById("customerName");
+var customerId = document.getElementById("customerId");
+var customerRelation = document.getElementById("customerRelation");
+// add Product Function
+function addCustomer() {
+  var customerData = {
+    name: customerName.value,
+    numbId: customerId.value,
+    relation: customerRelation.value,
+    
+
+  }
+  customerContainer.push(customerData);
+  clearForm();
+  displayProducts();
+}
+function clearForm() {
+  customerName.value = "";
+  customerId.value = "";
+  customerRelation.value = "";
+ 
+
+}
+function displayProducts() {
+  var cartona = ``;
+  for (var i = 0; i < customerContainer.length; i++) {
+    cartona += `
+        <tr>
+        <td>${customerContainer[i].name}</td>
+        <td>${customerContainer[i].numbId}</td>
+        <td>${customerContainer[i].relation}</td>
+   
+        <td><button onclick='deleteproduct(${i})' class="btn btn-outline-danger">مسح</button></td>
+        
+        </tr>`;
+  }
+  document.getElementById("tableBody").innerHTML = cartona;
+}
+function deleteproduct(index) {
+  customerContainer.splice(index, 1);
+  displayProducts();
+}
+            
