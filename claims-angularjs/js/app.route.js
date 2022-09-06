72f5fbb13c2c19cc6ce5d34b2852f652c1877c9f
ZinabHashem <zadel@informatique-eg.com>
2022-09-06 23:35:21 +0200

    court page

--- claims-angularjs/js/app.route.js
+++ claims-angularjs/js/app.route.js
@@ -1,10 +1,12 @@
 var app = angular.module('claimsApp', [
     'ngRoute',
+    'courtModule',
     'loginModule'
 ]);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/login', {templateUrl: 'auth/login.html', controller: 'LoginController'}).
+    $routeProvider.when('/court', {templateUrl: 'pages/court.html', controller: 'courtController'}).
+    when('/login', {templateUrl: 'auth/login.html', controller: 'LoginController'}).
     otherwise({
         redirectTo: '/login'
     });
