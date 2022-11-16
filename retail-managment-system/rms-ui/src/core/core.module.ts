212dfad268e7c48c4f8e98bf27babee9fb604a23
Nada407 <nadasharif775@gmail.com>
2022-11-16 11:50:41 +0200

    Deleting unused line

--- retail-managment-system/rms-ui/src/core/core.module.ts
+++ retail-managment-system/rms-ui/src/core/core.module.ts
@@ -1,7 +1,5 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 // import { MaterialModuleModule } from './material-module/material-module.module';
 
@@ -9,28 +7,17 @@ import { RouterModule } from '@angular/router';
   declarations: [],
   imports: [
     // MaterialModuleModule,
-    CommonModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule,
-    CommonModule,
+
     RouterModule,
   ],
-  exports: [
-    // MaterialModuleModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    CommonModule,
-  ],
+  exports: [],
 })
 export class CoreModule {
-    
-  constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
       throw new Error(
-        'CoreModule is already loaded. Import it in the AppModule only');
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
     }
   }
 }
-
