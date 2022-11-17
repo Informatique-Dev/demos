812374de49f00d9b311081726aa60960219f2a6c
abdelrahmanelsweedy <aelsweedy@sirius-its.com>
2022-11-17 09:14:11 +0200

    layout

--- retail-managment-system/rms-ui/src/app/core/core.module.ts
+++ retail-managment-system/rms-ui/src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { RootComponent } from './layout/root/root.component';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    RouterModule
-  ],
-  exports: []
+  declarations: [HeaderComponent, FooterComponent, RootComponent],
+  imports: [RouterModule, SharedModule, CommonModule],
+  exports: [SharedModule],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
