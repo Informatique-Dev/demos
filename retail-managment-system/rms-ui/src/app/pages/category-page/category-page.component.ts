a1fcfa40811091dc20bbcea77952a3ab1691fa28
Nada407 <nadasharif775@gmail.com>
2022-11-22 09:32:52 +0200

    Updating form and functions

--- retail-managment-system/rms-ui/src/app/pages/category-page/category-page.component.ts
+++ retail-managment-system/rms-ui/src/app/pages/category-page/category-page.component.ts
@@ -8,8 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPageComponent implements OnInit {
   categoryForm!: FormGroup;
+  allCategory: Category[] = [];
+  isAppear!: boolean;
+  pictureChanged: boolean = false;
+  displayedColumns: string[] = ['id', 'name', 'version', 'statue'];
+  dataimage: any;
+  submit: boolean = false;
+  submitted: boolean = false;
+  isVisible: boolean;
   constructor() {}
 
   ngOnInit(): void {}
+  resetTheForm(): void {
+    this.categoryForm.reset();
+    this.pictureChanged = false;
+  }
+  fetchData(candidate: Category): void {
+    this.resetTheForm();
+    this.categoryForm.patchValue({
+      id: candidate.id,
+      name: candidate.name,
+      email: candidate.email,
+      phone: candidate.phone,
+      documentId: candidate.documentId
+    });
+
   onSubmit() {}
+
+  appearRest() {
+    this.isAppear = true;
+  }
 }
