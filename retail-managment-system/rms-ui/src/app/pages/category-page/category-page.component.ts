240a23c4435dcff66080eaf5f5dd3fc023b3fe5a
Nada407 <nadasharif775@gmail.com>
2022-11-26 21:20:50 +0200

    Updates

--- retail-managment-system/rms-ui/src/app/pages/category-page/category-page.component.ts
+++ retail-managment-system/rms-ui/src/app/pages/category-page/category-page.component.ts
@@ -12,7 +12,6 @@ import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-category-page',
   templateUrl: './category-page.component.html',
-  styleUrls: ['./category-page.component.scss'],
 })
 export class CategoryPageComponent implements OnInit {
   categoryForm!: FormGroup;
