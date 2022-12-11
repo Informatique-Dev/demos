c27bff1d15c3a168526ced29f6d384371032e196
Nada407 <nadasharif775@gmail.com>
2022-12-11 11:19:46 +0200

    Solve Conflict

--- retail-managment-system/rms-ui/src/app/pages/pages.module.ts
+++ retail-managment-system/rms-ui/src/app/pages/pages.module.ts
@@ -1,11 +1,9 @@
-import { PopUpInvestorComponent } from './investor-page/pop-up/pop-up.component';
 import { InvestorPageComponent } from './investor-page/investor-page.component';
 import { NgModule } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { CategoryPageComponent } from './category-page/category-page.component';
-import { PopUpComponent } from './category-page/pop-up/pop-up.component';
 import { ProductComponent } from './product/product.component';
 import { CustomersComponent } from './customers/customers.component';
 
@@ -21,7 +19,6 @@ const routes: Routes = [
   declarations: [
     ProductComponent,
     CategoryPageComponent,
-    PopUpComponent,
     CustomersComponent,
     InvestorPageComponent,
   ],
