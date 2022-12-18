fffccf2df8c1187ae58b4772381cc619e1722070
abdelrahmanelsweedy <aelsweedy@sirius-its.com>
2022-12-18 15:57:18 +0200

    deleting menu & enhancement table bg-color

--- retail-managment-system/rms-ui/src/app/core/layout/root/root.component.ts
+++ retail-managment-system/rms-ui/src/app/core/layout/root/root.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './root.component.html',
   styles: [
-    '.side-nav { width: 200px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}',
+    '.side-nav { width: 200px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.5);}',
   ],
 })
 export class RootComponent {}
--- retail-managment-system/rms-ui/src/app/core/layout/side-bar/side-bar.component.ts
+++ retail-managment-system/rms-ui/src/app/core/layout/side-bar/side-bar.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styles: [
-    '.active { background-color: #002d40; color: white; padding: 10px;} .mat-divider { border-top-width: 3px; border-color: #002d40;  }',
+    '.active { background-color: #002d40; color: white; padding: 10px;} .mat-divider { border-top-width: 3px; border-color: #002d40; }',
   ],
 })
 export class SideBarComponent {
