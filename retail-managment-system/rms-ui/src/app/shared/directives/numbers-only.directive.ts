c70cdba56a03b7236ada881e602f74b0b137ac19
abdelrahmanelsweedy <aelsweedy@sirius-its.com>
2022-12-19 12:11:06 +0200

    deleting stopPropagation

--- retail-managment-system/rms-ui/src/app/shared/directives/numbers-only.directive.ts
+++ retail-managment-system/rms-ui/src/app/shared/directives/numbers-only.directive.ts
@@ -6,11 +6,8 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class NumbersOnlyDirective {
   constructor(private elementRef: ElementRef) {}
 
-  @HostListener('input', ['$event']) onInputChange(event: KeyboardEvent) {
+  @HostListener('input', ['$event']) onInputChange() {
     const initalValue = this.elementRef.nativeElement.value;
     this.elementRef.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
-    if (initalValue !== this.elementRef.nativeElement.value) {
-      event.stopPropagation();
-    }
   }
 }
