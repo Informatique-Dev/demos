36d5935cd518ea819d1ed2e7bbf17979f44ff8e7
AbdelrahmanMohyeldin <amohyeldin@sirius-its.com>
2023-01-15 17:10:21 +0200

    create core module init once

--- candi-meter/candi-meter-ui/src/app/app.module.ts
+++ candi-meter/candi-meter-ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
