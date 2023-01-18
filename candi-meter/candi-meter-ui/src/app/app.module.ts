9f54cd7e3db2426dfee66a8e27189c5cbaa9faa0
AbdelrahmanMohyeldin <amohyeldin@sirius-its.com>
2023-01-18 14:34:49 +0200

    deleted unused core module,fixed cdk install

--- candi-meter/candi-meter-ui/src/app/app.module.ts
+++ candi-meter/candi-meter-ui/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
