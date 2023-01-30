d0f973c0338fa6d199734e49c508c90185081019
pc <pc@DESKTOP-A2DOBGD>
2023-01-30 17:59:54 +0200

    Edit-InstallmentPage

--- retail-managment-system/rms-ui/src/app/pages/installments/installment/installment.component.ts
+++ retail-managment-system/rms-ui/src/app/pages/installments/installment/installment.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit , ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InstallmentRepositry } from 'src/app/domain/installment/installment.repositry';
 import { Installment } from 'src/app/domain/installment/models/installment';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatDialog } from '@angular/material/dialog';
-import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
 import { TranslateService } from '@ngx-translate/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -18,33 +14,26 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class InstallmentComponent implements OnInit  {
   installmentList:Installment[]=[];
-  installForm!: FormGroup;
-  submit: boolean = false;
-  currentData!: Installment;
-  isButtonVisible: boolean = true;
   @ViewChild(MatSort) sort!: MatSort 
   dataSource!: MatTableDataSource<Installment>;
+  searchText:string='';
   displayedColumns: string[] = [
     'id',
+    'customerName',
     'installmentAmount',
     'paymentAmount',
     'dueDate',
     'paymentDate',
     'status',
-    'actions',
   ];
 
   constructor(
     private installmentrepositry: InstallmentRepositry,
-    private formBuilder: FormBuilder,
-    private _snackBar: MatSnackBar,
-    private dialog: MatDialog,
     private translate: TranslateService
   ) {}
  
   ngOnInit(): void {
     this.getAllInstallments();
-    this.installmentForm();
   }
 
   getAllInstallments(): void {
@@ -55,104 +44,22 @@ export class InstallmentComponent implements OnInit  {
     });
   }
 
-  installmentForm() {
-    this.installForm = this.formBuilder.group({
-      id: [''],
-      installmentAmount: ['', [Validators.required]],
-      paymentAmount: ['', [Validators.required]],
-      dueDate: ['', [Validators.required]],
-      paymentDate: [''],
-      status: [''],
+
+  search()
+  {
+    if (!this.searchText)
+    {
+       this.getAllInstallments()
+    } 
+    
+     else
+    {
+     this.installmentList = this.installmentList.filter(res => {
+      this.dataSource = new MatTableDataSource (this.installmentList)
+      return res.order.customer.fullName.toLocaleLowerCase().match(this.searchText.toLocaleLowerCase())
     });
-  }
-
-  fetchData(install: Installment): void {
-    this.isButtonVisible = false;
-    this.installForm.patchValue(install);
-    this.currentData = install;
-  }
-
-  updateInstallment() {
-    this.isButtonVisible = true;
-    this.submit = true;
-    this.installmentrepositry.update(this.installForm.value).subscribe(
-      () => {
-        this.getAllInstallments();
-        this.submit = false;
-        this._snackBar.open(
-          this.translate.instant('installments.updated-successfuly'),
-          this.translate.instant('installments.close'),
-          {
-            duration: 2000,
-          }
-        );
-      },
-      () => {
-        this.submit = false;
-      }
-    );
-  }
-
-  addInstallment() {
-    this.isButtonVisible = true;
-    this.submit = true;
-    this.installmentrepositry.add(this.installForm.value).subscribe(
-      () => {
-        this.getAllInstallments();
-        this.submit = false;
-        this._snackBar.open(
-          this.translate.instant('installments.added-successfuly'),
-          this.translate.instant('installments.close'),
-          {
-            duration: 2000,
-          }
-        );
-      },
-      () => {
-        this.submit = false;
-      }
-    );
-  }
-
-  onSubmit() {
-    this.installForm.markAllAsTouched();
-    if (this.installForm.valid) {
-      this.installForm.controls['id'].value
-        ? this.updateInstallment()
-        : this.addInstallment();
-      this.installForm.reset();
+    
     }
-  }
-
-  resetForm() {
-    this.installForm.controls['id'].value
-      ? this.fetchData(this.currentData)
-      : this.installForm.reset();
-  }
-
-  restartForm(): void {
-    this.installForm.reset();
-  }
-
-  openConfirmationDialog(install: Installment) {
-    let dialogRef = this.dialog.open(ConfirmDialogComponent);
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result === 'yes') {
-        this.deleteInstallment(install);
-      }
-    });
-  }
-
-  deleteInstallment(install: Installment) {
-    this.installmentrepositry.delete(install.id).subscribe(() => {
-      this.getAllInstallments();
-      this._snackBar.open(
-        this.translate.instant('installments.delete-successfuly'),
-        this.translate.instant('installments.close'),
-        {
-          duration: 2000,
-        }
-      );
-    });
+   
   }
 }
