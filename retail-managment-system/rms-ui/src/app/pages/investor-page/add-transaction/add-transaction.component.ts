3df1d0835005992c6864f1fb87851ffc08bed670
pc <pc@DESKTOP-A2DOBGD>
2023-02-20 16:30:22 +0200

    final edit

--- retail-managment-system/rms-ui/src/app/pages/investor-page/add-transaction/add-transaction.component.ts
+++ retail-managment-system/rms-ui/src/app/pages/investor-page/add-transaction/add-transaction.component.ts
@@ -8,9 +8,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { Investors } from 'src/app/domain/investors/models/investor';
 import { InvestorsRepository } from 'src/app/domain/investors/investor.repository';
 
-
-
-
 @Component({
   selector: 'app-add-transaction',
   templateUrl: './add-transaction.component.html',
@@ -123,7 +120,5 @@ export class AddTransactionComponent implements OnInit {
   closeDialog(){
     this.dialog.closeAll();
   }
-
- 
 
 }
