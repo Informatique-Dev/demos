41b01fdc276ed473628ca0e6501d06d63772b6ca
Hemmat <mmhemat99@gmail.com>
2023-02-22 13:17:57 +0200

    create_crud_api_Order_Item

--- retail-managment-system/rms-api/src/main/java/com/rms/rest/dto/OrderItemDto.java
+++ retail-managment-system/rms-api/src/main/java/com/rms/rest/dto/OrderItemDto.java
@@ -1,13 +1,22 @@
 package com.rms.rest.dto;
 
 import com.rms.rest.dto.common.GenericDto;
+import com.rms.rest.validation.InsertValidation;
+import com.rms.rest.validation.UpdateValidation;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 @Data
 public class OrderItemDto extends GenericDto {
+    @NotBlank(message = "Price is mandatory", groups = {InsertValidation.class, UpdateValidation.class})
+    @Size(min = 1, message = "Price's min allowed is 1 ", groups = {InsertValidation.class, UpdateValidation.class})
 
     private Double unitPrice;
+    @NotBlank(message = "quantity is mandatory", groups = {InsertValidation.class, UpdateValidation.class})
+    @Size(min = 1, message = "quantity's min allowed is 1 ", groups = {InsertValidation.class, UpdateValidation.class})
     private Integer quantity;
     private Short paymentType;
     private ProductDto product;
--- retail-managment-system/rms-api/src/main/java/com/rms/service/OrderItemService.java
+++ retail-managment-system/rms-api/src/main/java/com/rms/service/OrderItemService.java
@@ -1,8 +1,11 @@
 package com.rms.service;
 
+import com.rms.domain.core.Employee;
 import com.rms.domain.sales.OrderItem;
 import com.rms.repository.OrderItemRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +17,7 @@ public class OrderItemService {
 
     private OrderItemRepository orderItemRepository;
 
-    public OrderItem save(OrderItem orderItem) {
+    public  OrderItem save(OrderItem orderItem) {
         return orderItemRepository.save(orderItem);
     }
 
@@ -22,14 +25,12 @@ public class OrderItemService {
         return orderItemRepository.findById(id);
     }
 
-    public List<OrderItem> getAll() {
-        return orderItemRepository.findAll();
+    public Page<OrderItem>  getAll(Integer page , Integer size){
+        return orderItemRepository.findAll(PageRequest.of(page,size));
     }
 
-
-
-    public void delete(OrderItem orderItem) {
-        orderItemRepository.delete(orderItem);
+    public void deleteById(Integer id) {
+        orderItemRepository.deleteById(id);
     }
 
 }
