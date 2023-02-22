8bf4e32c9a9a345acc8ca85419b14731d71fe4ee
Hams-Mohamed <hragaie@sirius-its.com>
2023-02-22 10:09:35 +0200

    #97 create crud api Bill

---
+++ retail-managment-system/rms-api/src/main/java/com/rms/repository/BillRepository.java
@@ -0,0 +1,9 @@
+package com.rms.repository;
+
+import com.rms.domain.purchase.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill,Integer> {
+}
