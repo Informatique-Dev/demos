84ab432ff760ee73924ac279271176d92f173751
AbdelrahmanMohyeldin <amohyeldin@sirius-its.com>
2023-03-08 17:06:54 +0200

    resolve conflict

--- retail-managment-system/rms-ui/src/app/pages/category-page/category-page.component.ts
+++ retail-managment-system/rms-ui/src/app/pages/category-page/category-page.component.ts
@@ -12,7 +12,7 @@ import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-category-page',
   templateUrl: './category-page.component.html',
-  styleUrls: ['./category-page.component.scss','../../shared/shared-styles.scss'],
+  styleUrls: ['./category-page.component.scss'],
 })
 export class CategoryPageComponent implements OnInit {
   categoryForm!: FormGroup;
