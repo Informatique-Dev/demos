ee33b55bbfb82e7df8c260606a73f596bd8481c0
AbdelrahmanMohyeldin <amohyeldin@sirius-its.com>
2023-03-15 19:53:41 +0200

    change prodFilter func

--- retail-managment-system/rms-ui/src/app/domain/product/product.repository.ts
+++ retail-managment-system/rms-ui/src/app/domain/product/product.repository.ts
@@ -3,15 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './models/product';
 import { CategoryRepository } from 'src/app/domain/category/category.repository';
-import { Category } from 'src/app/domain/category/models/category';
+import { SettingsService } from 'src/app/core/services/settings.service';
 import { Observable } from 'rxjs';
-import { Response } from '../../core/models/response';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductRepository extends ResourceService<Product> {
+  
+protected readonly productCategoryUrl =
+SettingsService.configurationEnvironment.api.baseUrl
   constructor(
     httpClient: HttpClient,
     public categoryRepository: CategoryRepository
@@ -21,8 +23,8 @@ export class ProductRepository extends ResourceService<Product> {
   getResourceUrl(): string {
     return 'product';
   }
-  filterProductsById(id:number):Observable<Response<Category>>{
-    return this.categoryRepository.getListById(`${id}/product`)
+  filterProductsById(id:number): Observable<Response>{
+    return this.httpClient.get<Response>(`${this.productCategoryUrl}category/${id}/product`)
   }
 
   toServerModel(entity: Product): any {
