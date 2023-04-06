abc020e00e615df891965968d7bc8ca49f00ef4d
Hemmat <hmofty@sirius-its.com>
2023-04-06 13:32:02 +0200

    #145_get all bill details by bill id

--- retail-managment-system/rms-api/src/main/java/com/rms/repository/BillDetailsRepository.java
+++ retail-managment-system/rms-api/src/main/java/com/rms/repository/BillDetailsRepository.java
@@ -1,9 +1,13 @@
 package com.rms.repository;
-
 import com.rms.domain.purchase.BillDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 @Repository
 public interface BillDetailsRepository extends JpaRepository<BillDetails, Integer> {
+    @Query("select b from BillDetails b where b.bill.id = :billId")
+    List<BillDetails> findBillDetailsByBillId(@Param("billId") Integer billId);
+
 }
--- retail-managment-system/rms-api/src/main/java/com/rms/rest/controller/BillDetailsController.java
+++ retail-managment-system/rms-api/src/main/java/com/rms/rest/controller/BillDetailsController.java
@@ -1,19 +1,14 @@
 package com.rms.rest.controller;
+
 import com.rms.rest.dto.BillDetailsDto;
-import com.rms.rest.dto.OrderItemDto;
 import com.rms.rest.handler.BillDetailsHandler;
-import com.rms.rest.validation.InsertValidation;
 import com.rms.rest.validation.UpdateValidation;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/bill-details")
@@ -21,28 +16,36 @@ import java.util.List;
 @Tag(name = "BillDetails", description = "Rest Api For BillDetails")
 public class BillDetailsController {
     private BillDetailsHandler billDetailsHandler;
+
     @GetMapping
-    @Operation(summary = "Get All" , description = "this api for get all BillDetails")
-    public ResponseEntity<?> getAll( @RequestParam (value = "page",defaultValue = "0") Integer page,
-                                     @RequestParam(value = "size",defaultValue = "10") Integer size){
-        return billDetailsHandler.getAll(page,size);
+    @Operation(summary = "Get All", description = "this api for get all BillDetails")
+    public ResponseEntity<?> getAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                    @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        return billDetailsHandler.getAll(page, size);
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Get By Id", description = "this api for get BillDetails by id")
-    public ResponseEntity<?> getById(@RequestParam("id") Integer id){
+    public ResponseEntity<?> getById(@RequestParam("id") Integer id) {
         return billDetailsHandler.getById(id);
+    }
+
+    @GetMapping("/bill/{billId}")
+    @Operation(summary = "Get BillDetails By Bill Id ", description = "this api for get all billDetails by order id")
+    public ResponseEntity<?> getByCustomerNationalId(@PathVariable("billId") Integer billId) {
+        return billDetailsHandler.findAllBillDetailsByBillId(billId);
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "update", description = "this api for update new BillDetails")
     public ResponseEntity<?> update
             (@Validated(UpdateValidation.class)
-           @RequestBody BillDetailsDto billDetailsDto,
+             @RequestBody BillDetailsDto billDetailsDto,
              @PathVariable Integer id
-            ){
-        return billDetailsHandler.update(billDetailsDto,id);
+            ) {
+        return billDetailsHandler.update(billDetailsDto, id);
     }
+
     @DeleteMapping("/{id}")
     @Operation(summary = "delete BillDetails By Id")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
