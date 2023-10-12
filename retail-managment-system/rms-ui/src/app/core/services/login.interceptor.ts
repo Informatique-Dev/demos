481d52fd103a45a2fc5034f988e38a7e96cb5816
HagerSayed22 <hsayed@sirius-its.com>
2023-10-12 15:05:35 +0300

    modified in the interceptor request token and handle error 401 for anyone who logIn

--- retail-managment-system/rms-ui/src/app/core/services/login.interceptor.ts
+++ retail-managment-system/rms-ui/src/app/core/services/login.interceptor.ts
@@ -3,22 +3,31 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
-export class LoginInterceptor implements HttpInterceptor {
-
-  constructor(@Inject(Injector) private readonly injector: Injector) {}
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const auth=this.injector.get(AuthService);
-    request.clone({
-        setHeaders: {
-        Authorization: `Bearer ${auth.getToken()}`
-      } 
-    })
-    return next.handle(request);
-  }
+export class LoginInterceptor implements HttpInterceptor{
+  constructor(@Inject(Injector) private readonly injector: Injector,private router:Router) {}
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+    if (err.status === 401) {
+        this.router.navigateByUrl(`/login`);
+    }
+    return throwError(err);
 }
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+     const auth=this.injector.get(AuthService);
+    let updateRequest=request.clone({
+      setHeaders: { Authorization: `Bearer ${auth.getToken()}` }
+    }) 
+    return next.handle(updateRequest).pipe(catchError(x=> this.handleAuthError(x)))
+  
+  }
+
+}
+
+
