42f159ed8ba6af3082855ad8ceacb29042cfc0a1
HagerSayed22 <hsayed@sirius-its.com>
2023-10-21 17:36:19 +0300

    removed link css in stepper.ts

--- parliament-vanguards-egypt/src/app/stepper/stepper.component.ts
+++ parliament-vanguards-egypt/src/app/stepper/stepper.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.scss']
 })
 export class StepperComponent {
   isLinear = true;
